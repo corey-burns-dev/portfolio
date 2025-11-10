c214739eaf71c69d895086dcea7612f14f5de2a6
Corey Burns <coreyburnsdev@aol.com>
2025-11-10 00:30:06 -0500

    feat: added all the relevant pages.  basic styles and navbar applied.  roughy copy complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,9 +10,6 @@ export default defineConfig({
   },
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'viewport',
-  },
-  experimental: {
-    clientPrerender: true,
+    defaultStrategy: 'hover',
   },
 });
